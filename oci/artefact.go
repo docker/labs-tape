d85b23f393956bfae3274b57989712b7e06f2a78
Ilya Dmitrichenko <errordeveloper@gmail.com>
2023-07-26 11:44:03 +0100

    Import `PushArtefact` from Flux

--- oci/artefact.go
+++ oci/artefact.go
@@ -4,6 +4,17 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/fluxcd/pkg/oci"
+	"github.com/google/go-containerregistry/pkg/crane"
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/empty"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/google/go-containerregistry/pkg/v1/types"
 )
 
 type ArtefactInfo struct {
@@ -47,4 +58,54 @@ func (c *Client) GetArtefact(ctx context.Context, ref string) (*ArtefactInfo, er
 	}
 
 	return info, nil
+}
+
+// based on https://github.com/fluxcd/pkg/blob/2a323d771e17af02dee2ccbbb9b445b78ab048e5/oci/client/push.go
+func (c *Client) PushArtefact(ctx context.Context, url, sourceDir string, meta Metadata, ignorePaths []string) (string, error) {
+	ref, err := name.ParseReference(url)
+	if err != nil {
+		return "", fmt.Errorf("invalid URL: %w", err)
+	}
+
+	tmpDir, err := os.MkdirTemp("", "oci")
+	if err != nil {
+		return "", err
+	}
+	defer os.RemoveAll(tmpDir)
+
+	tmpFile := filepath.Join(tmpDir, "artifact.tgz")
+
+	if err := c.Build(tmpFile, sourceDir, ignorePaths); err != nil {
+		return "", err
+	}
+
+	if meta.Created == "" {
+		ct := time.Now().UTC()
+		meta.Created = ct.Format(time.RFC3339)
+	}
+
+	img := mutate.MediaType(empty.Image, types.OCIManifestSchema1)
+	img = mutate.ConfigMediaType(img, oci.CanonicalConfigMediaType)
+	img = mutate.Annotations(img, meta.ToAnnotations()).(gcrv1.Image)
+
+	layer, err := tarball.LayerFromFile(tmpFile, tarball.WithMediaType(oci.CanonicalContentMediaType))
+	if err != nil {
+		return "", fmt.Errorf("creating content layer failed: %w", err)
+	}
+
+	img, err = mutate.Append(img, mutate.Addendum{Layer: layer})
+	if err != nil {
+		return "", fmt.Errorf("appeding content to artifact failed: %w", err)
+	}
+
+	if err := crane.Push(img, url, c.optionsWithContext(ctx)...); err != nil {
+		return "", fmt.Errorf("pushing artifact failed: %w", err)
+	}
+
+	digest, err := img.Digest()
+	if err != nil {
+		return "", fmt.Errorf("parsing artifact digest failed: %w", err)
+	}
+
+	return ref.Context().Digest(digest.String()).String(), err
 }
