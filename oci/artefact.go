d770bc2ba71ebbe8daaeef228d15ff57a2593b77
Ilya Dmitrichenko <errordeveloper@gmail.com>
2023-07-26 11:12:49 +0100

    Split artefact-specific functions to a separate file

---
+++ oci/artefact.go
@@ -0,0 +1,50 @@
+package oci
+
+import (
+	"context"
+	"fmt"
+	"io"
+)
+
+type ArtefactInfo struct {
+	io.ReadCloser
+
+	MediaType   MediaType
+	Annotations map[string]string
+}
+
+func (c *Client) GetArtefact(ctx context.Context, ref string) (*ArtefactInfo, error) {
+	image, err := c.Pull(ctx, ref)
+	if err != nil {
+		return nil, fmt.Errorf("failed to pull %q: %w", ref, err)
+	}
+	manifest, err := image.Manifest()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get manifest of %q: %w", ref, err)
+	}
+	if len(manifest.Layers) < 1 {
+		return nil, fmt.Errorf("no layers found in image %q", ref)
+	}
+	if len(manifest.Layers) > 1 {
+		return nil, fmt.Errorf("multiple layers found in image %q", ref)
+	}
+	layerDecriptor := manifest.Layers[0]
+
+	layer, err := image.LayerByDigest(layerDecriptor.Digest)
+	if err != nil {
+		return nil, fmt.Errorf("fetching aretefact image failed: %w", err)
+	}
+
+	blob, err := layer.Uncompressed()
+	if err != nil {
+		return nil, fmt.Errorf("extracting uncompressed aretefact image failed: %w", err)
+	}
+
+	info := &ArtefactInfo{
+		ReadCloser:  blob,
+		MediaType:   layerDecriptor.MediaType,
+		Annotations: layerDecriptor.Annotations,
+	}
+
+	return info, nil
+}
--- oci/oci.go
+++ oci/oci.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"strings"
 
 	ociclient "github.com/fluxcd/pkg/oci/client"
@@ -94,49 +93,6 @@ func (c *Client) ListRelated(ctx context.Context, ref, digest string) ([]Metadat
 		return nil, err
 	}
 	return tags, nil
-}
-
-type ArtefactInfo struct {
-	io.ReadCloser
-
-	MediaType   MediaType
-	Annotations map[string]string
-}
-
-func (c *Client) GetArtefact(ctx context.Context, ref string) (*ArtefactInfo, error) {
-	image, err := c.Pull(ctx, ref)
-	if err != nil {
-		return nil, fmt.Errorf("failed to pull %q: %w", ref, err)
-	}
-	manifest, err := image.Manifest()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get manifest of %q: %w", ref, err)
-	}
-	if len(manifest.Layers) < 1 {
-		return nil, fmt.Errorf("no layers found in image %q", ref)
-	}
-	if len(manifest.Layers) > 1 {
-		return nil, fmt.Errorf("multiple layers found in image %q", ref)
-	}
-	layerDecriptor := manifest.Layers[0]
-
-	layer, err := image.LayerByDigest(layerDecriptor.Digest)
-	if err != nil {
-		return nil, fmt.Errorf("fetching aretefact image failed: %w", err)
-	}
-
-	blob, err := layer.Uncompressed()
-	if err != nil {
-		return nil, fmt.Errorf("extracting uncompressed aretefact image failed: %w", err)
-	}
-
-	info := &ArtefactInfo{
-		ReadCloser:  blob,
-		MediaType:   layerDecriptor.MediaType,
-		Annotations: layerDecriptor.Annotations,
-	}
-
-	return info, nil
 }
 
 func IsCosignArtifact(ref string) bool {
